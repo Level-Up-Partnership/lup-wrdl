770f1743225c5c73d79c43d24713d5a00cffebde
StendaH <edwardson.pascual@gmail.com>
2026-07-06 13:05:48 -0400

    WRDL-47: Test isGuessTooShort returns false when guess is longer than wordLength

--- src/gameLogic.test.js
+++ src/gameLogic.test.js
@@ -19,6 +19,16 @@ describe( 'isGuessTooShort', () => {
 
     } )
 
+    // WRDL-47: Test isGuessTooShort returns false when guess is longer than wordLength
+    it( 'returns false when guess is longer than wordLength', () => {
+
+        const guess = 'CATS'
+        const wordLength = 3
+
+        expect( isGuessTooShort( guess, wordLength ) ).toBe( false )
+
+    } )
+
 } )
 
 
